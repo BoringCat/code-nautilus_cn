5ea2d309f1d5ae83a0ef6586015b56a52419c949
boringcat <c654477757@gmail.com>
2020-03-09 10:48:35 +0800

    合并语言配置

--- install.sh
+++ install.sh
@@ -11,11 +11,38 @@ cd $(dirname `realpath $0`)
 # Get $LANG
 LANG_=$(echo $LANG | cut -d '.' -f1)
 if [ $LANG_ == 'zh_CN' ]; then
-    source lang_zh_CN.sh
+    BEGIN_INSTALL="开始安装 python-nautilus 包..."
+    ARCH_DEBIAN_ALREADY_INSTALL="python-nautilus 已安装"
+    REDHAT_ALREADY_INSTALL="nautilus-python 已安装"
+    PKGNOTFOUND="找不到包 python-nautilus, 请手动安装."
+    REMOVE_OLD="删除旧版本（如果存在）..."
+    DOWNLOAD_NEW="下载最新版..."
+    RESTART_REQUIRE="完成安装需要关闭所有 nautilus 窗口, 确定?"
+    RESTART="正在关闭所有 nautilus 窗口..."
+    RESTART_NOCONFIRM="没有关闭所有 nautilus 窗口! 请手动关闭以确保已安装."
+    COMPLETE="安装完成"
 elif [ $LANG_ == 'zh_HK' ]; then
-    source lang_zh_HK.sh
+    BEGIN_INSTALL="開始裝 python-nautilus 包..."
+    ARCH_DEBIAN_ALREADY_INSTALL="python-nautilus 裝佐了"
+    REDHAT_ALREADY_INSTALL="nautilus-python 裝佐了"
+    PKGNOTFOUND="揾唔到 python-nautilus 包, 你要自己裝."
+    REMOVE_OLD="刪除舊版本（如果有嘅話）..."
+    DOWNLOAD_NEW="下載最新版..."
+    RESTART_REQUIRE="完成安裝需要關佐所有 nautilus 窗口, 確定?"
+    RESTART="關梗所有 nautilus 窗口..."
+    RESTART_NOCONFIRM="冇去關 nautilus 嘅窗口! 請手動關閉以確保安裝完成."
+    COMPLETE="搞掂"
 else
-    source lang_en_US.sh
+    BEGIN_INSTALL="Installing python-nautilus..."
+    ARCH_DEBIAN_ALREADY_INSTALL="python-nautilus is already installed."
+    REDHAT_ALREADY_INSTALL="nautilus-python is already installed."
+    PKGNOTFOUND="Failed to find python-nautilus, please install it manually."
+    REMOVE_OLD="Removing previous version (if found)..."
+    DOWNLOAD_NEW="Downloading newest version..."
+    RESTART_REQUIRE="We need restart all nautilus windows, confirm?"
+    RESTART="Restarting nautilus..."
+    RESTART_NOCONFIRM="No confirm! Please restart nautilus manually."
+    COMPLETE="Installation Complete"
 fi
 
 # Install python-nautilus
